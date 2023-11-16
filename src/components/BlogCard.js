d7c65fd8b9b3a4545d72dde353045bf73b8c13d2
parab.smita <parab.smita.s11@gmail.com>
2023-11-16 12:40:48 +0530

    blog page added

--- src/components/BlogCard.js
+++ src/components/BlogCard.js
@@ -3,21 +3,19 @@ import { Link } from 'react-router-dom'
 
 export const BlogCard = () => {
   return (
-    <div className="col-3">
-        <div className="blog-card card-wrapper">
-            <div className="card-image" >
-                <img src="images/blog-1.jpg" className="img-fluid" alt="blog" />
-            </div>    
-            <div className="blog-content">
-                <p className="date">1 April, 2023</p>
-                <h5 className="title">A beautiful sunday morning renaiance</h5>
-                <p className="desc">
-                    Lorem Ipsum is simply dummy text of the printing and typesetting
-                </p>
-                <Link to="/" className="button">
-                    Read More
-                </Link>
-            </div>
+    <div className="blog-card card-wrapper">
+        <div className="card-image" >
+            <img src="images/blog-1.jpg" className="img-fluid w-100" alt="blog" />
+        </div>    
+        <div className="blog-content">
+            <p className="date">1 April, 2023</p>
+            <h5 className="title">A beautiful sunday morning renaiance</h5>
+            <p className="desc">
+                Lorem Ipsum is simply dummy text of the printing and typesetting
+            </p>
+            <Link to="/" className="button">
+                Read More
+            </Link>
         </div>
     </div>
   )
